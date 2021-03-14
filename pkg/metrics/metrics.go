95c3214ae385d3a251ee7a9459ac0fdd518b929b
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2021-03-14 17:51:04 +0000

    Add total time and bandwidth saved

--- pkg/metrics/metrics.go
+++ pkg/metrics/metrics.go
@@ -2,6 +2,7 @@ package metrics
 
 import (
 	"fmt"
+	"net/url"
 	"strings"
 	"sync"
 	"time"
@@ -25,25 +26,53 @@ func NewMetrics() (metrics *Metrics) {
 	return metrics
 }
 
-func (metrics *Metrics) addTimeSaved(reqURL string, timeSaved time.Duration) {
-	currentTimeSavedInterface, _ := metrics.timeSaved.LoadOrStore(
+func (metrics *Metrics) addTimeSaved(
+	reqURL string,
+	timeSaved time.Duration,
+) (err error) {
+	url, err := url.Parse(reqURL)
+	if err != nil {
+		return err
+	}
+
+	hostname := url.Hostname()
+	hostnameTimeSavedInterface, _ := metrics.timeSaved.LoadOrStore(
+		hostname,
+		&sync.Map{},
+	)
+	hostnameTimeSaved := hostnameTimeSavedInterface.(*sync.Map)
+	fullPathTimeSavedInterface, _ := hostnameTimeSaved.LoadOrStore(
 		reqURL,
 		time.Duration(0),
 	)
-	currentTimeSaved := currentTimeSavedInterface.(time.Duration)
-	metrics.timeSaved.Store(reqURL, currentTimeSaved+timeSaved)
+	fullPathTimeSaved := fullPathTimeSavedInterface.(time.Duration)
+	hostnameTimeSaved.Store(reqURL, fullPathTimeSaved+timeSaved)
+
+	return nil
 }
 
 func (metrics *Metrics) addBandwidthSaved(
 	reqURL string,
 	bandwidthSaved int64,
-) {
-	currentBandwidthSavedInterface, _ := metrics.bandwidthSaved.LoadOrStore(
+) (err error) {
+	url, err := url.Parse(reqURL)
+	if err != nil {
+		return err
+	}
+	hostname := url.Hostname()
+	hostnameBandwitdhSavedInterface, _ := metrics.bandwidthSaved.LoadOrStore(
+		hostname,
+		&sync.Map{},
+	)
+	hostnameBandwidthSaved := hostnameBandwitdhSavedInterface.(*sync.Map)
+	fullPathBandwidthSavedInterface, _ := hostnameBandwidthSaved.LoadOrStore(
 		reqURL,
 		int64(0),
 	)
-	currentBandwidthSaved := currentBandwidthSavedInterface.(int64)
-	metrics.bandwidthSaved.Store(reqURL, currentBandwidthSaved+bandwidthSaved)
+	fullPathBandwidthSaved := fullPathBandwidthSavedInterface.(int64)
+	hostnameBandwidthSaved.Store(reqURL, fullPathBandwidthSaved+bandwidthSaved)
+
+	return nil
 }
 
 // AddMetrics calculates the time and bandwidth saved and adds them to the total
@@ -67,13 +96,39 @@ func (metrics *Metrics) String() string {
 	fmt.Fprintf(&builder, "%smetrics:\n", prefix)
 	prefix = "   "
 	fmt.Fprintf(&builder, "%stime saved:\n", prefix)
-	metrics.timeSaved.Range(func(key, value interface{}) bool {
-		fmt.Fprintf(&builder, "%s - %q: %s\n", prefix, key, value)
+	metrics.timeSaved.Range(func(hostname, fullPathTimeSavedInterface interface{}) bool {
+		fullPathTimeSaved := fullPathTimeSavedInterface.(*sync.Map)
+		totalTimeSaved := time.Duration(0)
+		fullPathTimeSaved.Range(func(fullPathName, timeSavedInterface interface{}) bool {
+			timeSaved := timeSavedInterface.(time.Duration)
+			totalTimeSaved += timeSaved
+			return true
+		})
+		fmt.Fprintf(&builder, "%s - %q: %s\n", prefix, hostname, totalTimeSaved)
+		prefix = "        "
+		fullPathTimeSaved.Range(func(fullPathName, timeSaved interface{}) bool {
+			fmt.Fprintf(&builder, "%s - %q: %s\n", prefix, fullPathName, timeSaved)
+			return true
+		})
+		prefix = "   "
 		return true
 	})
 	fmt.Fprintf(&builder, "%sbandwidth saved:\n", prefix)
-	metrics.bandwidthSaved.Range(func(key, value interface{}) bool {
-		fmt.Fprintf(&builder, "%s - %q: %v bytes\n", prefix, key, value)
+	metrics.bandwidthSaved.Range(func(hostname, fullPathBandwidthSavedInterface interface{}) bool {
+		fullPathBandwidthSaved := fullPathBandwidthSavedInterface.(*sync.Map)
+		totalBandwidthSaved := int64(0)
+		fullPathBandwidthSaved.Range(func(fullPathName, bandwidthSavedInterface interface{}) bool {
+			bandwidthSaved := bandwidthSavedInterface.(int64)
+			totalBandwidthSaved += bandwidthSaved
+			return true
+		})
+		fmt.Fprintf(&builder, "%s - %q: %v bytes\n", prefix, hostname, totalBandwidthSaved)
+		prefix = "        "
+		fullPathBandwidthSaved.Range(func(fullPathName, bandwidthSaved interface{}) bool {
+			fmt.Fprintf(&builder, "%s - %q: %v bytes\n", prefix, fullPathName, bandwidthSaved)
+			return true
+		})
+		prefix = "   "
 		return true
 	})
 
